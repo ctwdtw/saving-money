a5f2ab0e19451e8cc24e5bc6781f410893317697
ctwdtw <ctwdtw@gmail.com>
2021-12-15 14:01:57 +0800

    move `WriteDownPlanViewModel` and `KeyboardController` to separate files

--- SavingMoney/WritePlan/ViewController/WriteDownPlanViewController.swift
+++ SavingMoney/WritePlan/ViewController/WriteDownPlanViewController.swift
@@ -7,64 +7,6 @@
 
 import UIKit
 
-class KeyboardController: NSObject {
-    weak var view: UIView!
-    
-    weak var planTextField: UITextField!
-    
-    private let viewModel: KeyboardEventViewModel
-    
-    init(viewModel: KeyboardEventViewModel) {
-        self.viewModel = viewModel
-    }
-    
-    func bindKeyboardEvents() {
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(keyboardWillShow(notification:)),
-            name: UIControl.keyboardWillShowNotification,
-            object: nil
-        )
-        
-        viewModel.onKeyboardWillShow = { offset in
-            UIView.animate(withDuration: 0.3) {
-                self.view.frame.origin = CGPoint(x: 0, y: offset)
-            }
-        }
-        
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(keyboardWillHide),
-            name: UIControl.keyboardWillHideNotification,
-            object: nil
-        )
-        
-        viewModel.onKeyboardWillHide = {
-            UIView.animate(withDuration: 0.3) {
-                self.view.frame.origin = CGPoint(x: 0, y: 0)
-            }
-        }
-    }
-    
-    @objc private func keyboardWillShow(notification: Notification) {
-        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
-            return
-        }
-        
-        let keyboardTop = keyboardFrame.origin.y
-        let targetViewBottom = planTextField.convert(planTextField.bounds.origin, to: view).y
-        + planTextField.bounds.size.height
-        
-        viewModel.keyboardWillShow(keyboardTop: keyboardTop, targetViewBottom: targetViewBottom)
-        
-    }
-    
-    @objc private func keyboardWillHide() {
-        viewModel.keyboardWillHide()
-    }
-
-}
-
 public class WriteDownPlanViewController: UIViewController {
     @IBOutlet public private(set) weak var nextBarBtnItem: UIBarButtonItem!
     
--- SavingMoney/WritePlan/ViewModel/WriteDownPlanViewModel.swift
+++ SavingMoney/WritePlan/ViewModel/WriteDownPlanViewModel.swift
@@ -6,28 +6,6 @@
 //
 
 import Foundation
-import CoreGraphics
-
-class KeyboardEventViewModel {
-    var onKeyboardWillShow: ((CGFloat) -> Void)?
-    
-    func keyboardWillShow(keyboardTop: CGFloat, targetViewBottom: CGFloat) {
-        let spacing: CGFloat = 20.0
-        
-        let diff = keyboardTop - targetViewBottom
-        
-        if diff < spacing {
-            let offset = -spacing - abs(diff)
-            onKeyboardWillShow?(offset)
-        }
-    }
-    
-    var onKeyboardWillHide: (() -> Void)?
-    func keyboardWillHide() {
-        onKeyboardWillHide?()
-    }
-}
-
 class WriteDownPlanViewModel {
     var onNextStateChange: ((Bool) -> Void)?
     
